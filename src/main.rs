c19a9effc7f3e07f3e08b608e65a364d3bf7c150
Liam Dyer <liamcdyer@gmail.com>
2025-11-27 19:18:46 -0500

    feat: simplify task handling

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use anyhow::Result;
 use pallas::ledger::traverse::MultiEraBlock;
 use pallas::network::facades::NodeClient;
-use pallas::network::miniprotocols::Point;
 use pallas::network::miniprotocols::chainsync::{BlockContent, NextResponse, Tip};
+use pallas::network::miniprotocols::{Point, chainsync};
 use tokio::signal;
 use tokio::sync::mpsc;
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
 
 mod db;
 mod tx;
 use db::Db;
-use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
 
 const BUFFER_SIZE: usize = 10000;
 const NODE_SOCKET: &str = "./db/node/socket";
@@ -21,13 +21,8 @@ enum ProcessorEvent {
     RollForward(BlockContent, Tip),
     /// Rolled back to a point in the chain
     RollBackward(Point),
-    /// Reached the tip of the chain, waiting for new blocks
-    Await,
-    /// Program shutting down
-    Shutdown,
 }
 
-// TODO: safe shutdown on error
 #[tokio::main]
 async fn main() -> Result<()> {
     let subscriber = FmtSubscriber::builder()
@@ -36,10 +31,20 @@ async fn main() -> Result<()> {
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     let db = Db::new("./db/hydrant")?;
+    let peer = setup_node_connection(&db).await?;
+    let result = run_sync_loop(peer, db.clone()).await;
 
+    tracing::info!("Persisting database...");
+    db.persist()?;
+
+    result
+}
+
+async fn setup_node_connection(db: &Db) -> Result<NodeClient> {
     tracing::info!("Connecting to node...");
     let mut peer = NodeClient::connect(NODE_SOCKET, MAGIC).await?;
     let client = peer.chainsync();
+
     if let Ok(Some(tip)) = db.tip() {
         tracing::info!(?tip, "Requesting intersection");
         client.find_intersect(vec![tip]).await?;
@@ -47,17 +52,18 @@ async fn main() -> Result<()> {
         tracing::info!("No tip, starting from origin");
         client.intersect_origin().await?;
     }
-    tracing::info!("Connected to node");
 
-    // Process new blocks in a background task
+    tracing::info!("Connected to node");
+    Ok(peer)
+}
+
+async fn run_sync_loop(mut peer: NodeClient, db: Db) -> Result<()> {
     let (tx, mut rx) = mpsc::channel::<ProcessorEvent>(BUFFER_SIZE);
-    let processor_db = db.clone();
-    let processor_task = tokio::spawn(async move {
-        while let Some(response) = rx.recv().await {
-            if matches!(response, ProcessorEvent::Shutdown) {
-                return Ok(());
-            }
-            if let Err(error) = process_event(response, &processor_db) {
+
+    // Push blocks to database on a background task
+    let processor = tokio::spawn(async move {
+        while let Some(event) = rx.recv().await {
+            if let Err(error) = process_event(event, &db) {
                 tracing::error!(?error, "error processing chain-sync response");
                 return Err(error);
             }
@@ -65,38 +71,41 @@ async fn main() -> Result<()> {
         Ok(())
     });
 
-    tokio::select! {
-        // Fetch blocks from node
-        result = async {
-            loop {
-                if processor_task.is_finished() {
-                    tracing::error!("Processor task exited early, persisting database and exiting...");
-                    db.persist()?;
-                    return Ok(());
-                }
-
-                let next = match client.has_agency() {
-                    true => client.request_next().await?,
-                    false => client.recv_while_must_reply().await?,
-                };
-                tx.send(match next {
-                    NextResponse::RollForward(cbor, tip) => ProcessorEvent::RollForward(cbor, tip),
-                    NextResponse::RollBackward(point, _) => ProcessorEvent::RollBackward(point),
-                    NextResponse::Await => ProcessorEvent::Await,
-                }).await?;
-            }
-        } => {
-            result
-        }
-
-        // Shutdown
+    // Listen for chain-sync events until shutdown or error
+    let sync_result = tokio::select! {
+        res = fetch_chainsync_event(peer.chainsync(), tx.clone()) => res,
         _ = shutdown_signal() => {
             tracing::info!("Received shutdown signal, flushing processor...");
-            tx.send(ProcessorEvent::Shutdown).await?;
-            let _ = processor_task.await;
-            tracing::info!("Persisting database...");
-            db.persist()?;
             Ok(())
+        }
+    };
+
+    // Stop sending new events and wait for processor to drain
+    drop(tx);
+    let _ = processor.await?; // TODO: handle error
+
+    sync_result
+}
+
+async fn fetch_chainsync_event(
+    client: &mut chainsync::N2CClient,
+    tx: mpsc::Sender<ProcessorEvent>,
+) -> Result<()> {
+    loop {
+        let next = match client.has_agency() {
+            true => client.request_next().await?,
+            false => client.recv_while_must_reply().await?,
+        };
+
+        let event = match next {
+            NextResponse::RollForward(cbor, tip) => ProcessorEvent::RollForward(cbor, tip),
+            NextResponse::RollBackward(point, _) => ProcessorEvent::RollBackward(point),
+            NextResponse::Await => continue,
+        };
+
+        // If send fails, receiver has been dropped, so we should stop
+        if tx.send(event).await.is_err() {
+            return Ok(());
         }
     }
 }
@@ -108,8 +117,13 @@ fn process_event(event: ProcessorEvent, db: &Db) -> Result<()> {
             db.roll_forward(&block)?;
 
             let tip_slot = tip.0.slot_or_default();
-            if tip_slot < block.slot().saturating_sub(1000) || block.number() % 1000 == 0 {
-                tracing::info!(slot = block.slot(), number = block.number(), "RollForward");
+            if tip_slot.saturating_sub(1000) < block.slot() || block.number() % 1000 == 0 {
+                tracing::info!(
+                    slot = block.slot(),
+                    block = block.number(),
+                    ?tip_slot,
+                    "RollForward"
+                );
             }
 
             Ok(())
@@ -122,7 +136,6 @@ fn process_event(event: ProcessorEvent, db: &Db) -> Result<()> {
             };
             Ok(())
         }
-        _ => Ok(()),
     }
 }
 
